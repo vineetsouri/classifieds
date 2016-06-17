c2c5ad86b439db61977cb3b643334f84d155802a
vineetsouri <vineetsouri72@gmail.com>
2016-06-17 14:57:03 +0530

    adds changes to controllers to get data

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,12 @@
 class CategoriesController < ApplicationController
+  skip_before_filter :authenticate_user!, only: [:index, :show]
   def index
     @categories = Category.all
   end
 
   def show
     category = Category.find(params[:id])
-    @posts = Post.where("category_id = ?", category.id)
+    @posts = Post.where(category_id:  category.id).order(id: :desc)
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,42 @@
 class PostsController < ApplicationController
-  before_action :fetch_user, only: [:create]
+  skip_before_filter :authenticate_user!, only: [:index, :show]
+  before_action :fetch_categories, only: [:new, :create, :edit, :update]
   
   def new
-    @post = Post.new
+    @post = current_user.posts.new
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
+    if @post.save!
+      redirect_to posts_path
+    else
+      render :new
+    end 
+  end
+
+  def index
+    @posts = current_user.posts.order(id: :desc)
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to posts_path
   end
 
   private
 
-  def post_params
-    params.require(:post).permit(:title, :category_id, :description, 
-                                 :contact_name, :contact_no, :contact_email)
-  end
+    def post_params
+      params.require(:post).permit(:title, :category_id, :description, 
+                                   :contact_name, :contact_no, :contact_email)
+    end
 
-  def fetch_user
-    @user = current_user
-  end
+    def fetch_categories
+      @categories = Category.all
+    end
 end
